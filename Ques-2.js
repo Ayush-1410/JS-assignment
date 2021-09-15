c4c0eda0bfa873f6075db9457623fc994ccb0c1a
Ayush-1410 <63532654+Ayush-1410@users.noreply.github.com>
2021-09-15 15:20:16 +0530

    Add files via upload

---
+++ Ques-2.js
@@ -0,0 +1,21 @@
+function Palindrome(str) {
+
+    let len = string.length;
+
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return('It is not a palindrome');
+        }
+    }
+    return('It is a palindrome');
+}
+
+// take input
+let string = prompt('Enter a string: ');
+
+// function call
+const value = Palindrome(string);
+
+console.log(value);
---
+++ Ques-4.js
@@ -0,0 +1,9 @@
+// creating object data
+let data = {
+	name: 'Aadi',
+	age: 26,
+	sirname: 'Sawant'
+};
+
+let copy_data = data;
+console.log(copy_data);
